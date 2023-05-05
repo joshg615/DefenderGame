ca8a255b00ca39b898f0d1d35ad91ef7bda69c64
jorotyn <73930194+jorotyn@users.noreply.github.com>
2023-05-05 23:30:26 +1000

    Removed redundant buttons from DebuggingButtons.cs

--- Defender/Assets/Scripts/DebuggingButtons.cs
+++ Defender/Assets/Scripts/DebuggingButtons.cs
@@ -11,12 +11,6 @@ public class DebuggingButtons : MonoBehaviour
     // The vector3 force to apply when the Add Force button is pressed
     public Vector3 forceVector = new Vector3(1f, 0f, 0f);
 
-    // The vector3 movement to set when the Set Movement button is pressed
-    public Vector3 movementVector = new Vector3(1f, 0f, 0f);
-
-    // The vector3 position to move to when the Move Position button is pressed
-    public Vector3 positionVector = new Vector3(1f, 0f, 0f);
-
     private void OnGUI()
     {
         // Create a button with the label "Add Force" at position (10, 10) on the screen
@@ -24,20 +18,6 @@ public class DebuggingButtons : MonoBehaviour
         {
             // Call the AddForce method in the player controller with the specified force vector
             playerController.AddForce(forceVector);
-        }
-
-        // Create a button with the label "Set Movement" at position (10, 70) on the screen
-        if (GUI.Button(new Rect(10, 70, 100, 50), "Set Movement"))
-        {
-            // Call the SetMovement method in the player controller with the specified movement vector
-            playerController.SetMovement(movementVector);
-        }
-
-        // Create a button with the label "Move Position" at position (10, 130) on the screen
-        if (GUI.Button(new Rect(10, 130, 100, 50), "Move Position"))
-        {
-            // Call the MovePosition method in the player controller with the specified position vector
-            playerController.MovePosition(positionVector);
         }
     }
 }
